f8af0524681145ad752b093c80f75c62356a0864
sennen.ekouaghe <ekougs@gmail.com>
2013-09-27 00:04:41 +0200

    Insertion sort

--- src/main/java/com/algos/sort/InsertionSorter.java
+++ src/main/java/com/algos/sort/InsertionSorter.java
@@ -10,12 +10,20 @@ import static com.algos.sort.SorterHelper.lesser;
 
 public class InsertionSorter<T extends Comparable<T>> implements Sorter<T> {
     public void sort(T[] tableToSort) {
-        for (int i = 0; i < tableToSort.length; i++) {
-            for (int j = i; j > 0 && lesser(tableToSort[j], tableToSort[j - 1]); j--) {
-                T element = tableToSort[j];
-                tableToSort[j] = tableToSort[j - 1];
-                tableToSort[j - 1] = element;
-            }
+        for (int position = 0; position < tableToSort.length; position++) {
+            shiftLeftEveryElementLesserThanPreviousFrom(position, tableToSort);
         }
+    }
+
+    private void shiftLeftEveryElementLesserThanPreviousFrom(int i, T[] tableToSort) {
+        for (int j = i; j > 0 && lesser(tableToSort[j], tableToSort[j - 1]); j--) {
+            shiftLeftElement(tableToSort, j);
+        }
+    }
+
+    private void shiftLeftElement(T[] tableToSort, int j) {
+        T element = tableToSort[j];
+        tableToSort[j] = tableToSort[j - 1];
+        tableToSort[j - 1] = element;
     }
 }
